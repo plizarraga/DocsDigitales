319d26d0806cef3a261f8640dd55db68955b28fc
plizarraga <plizarraga86@gmail.com>
2014-07-17 14:56:13 -0700

    Eliminacion de BootstrapValidator

---
+++ DocsDigitalesApp/DocsDigitalesApp/Scripts/login.js
@@ -0,0 +1,39 @@
+﻿
+function ValidarRequeridos() {
+
+    $("#btnIniciarSesion").on("click", function () {
+
+        if ($("#correoElectronico").val() == "") {
+
+            $("#correoElectronico").addClass('error');
+            $("#errorCorreo").show();
+            return false;
+        }
+        else {
+            $("#correoElectronico").removeClass('error');
+            $("#errorCorreo").hide();
+            if (!validateEmail($("#correoElectronico").val())) { }
+        };
+
+        if ($("#contrasena").val() == "") {
+
+            $("#contrasena").addClass('error');
+            $("#errorContrasena").show();
+            return false;
+        }
+        else {
+            $("#contrasena").removeClass('error');
+            $("#errorContrasena").show();
+        };
+
+    });
+};
+
+function validateEmail(email) {
+    var emailReg = "/^([\w-\.]+@@([\w-]+\.)+[\w-]{2,4})?$/";
+    if (!emailReg.test(email)) {
+        return false;
+    } else {
+        return true;
+    }
+};
