100e09b4ddd305e1dd59a74a305ea12aef589aa8
plizarraga <plizarraga86@gmail.com>
2014-07-17 09:52:05 -0700

    Agregando Bootstrap y sus dependencias
    Se agrego el modelo y controlador de autentificacion

---
+++ DocsDigitalesApp/DocsDigitalesApp/Controllers/AutentificarController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocsDigitalesApp.Controllers
+{
+    public class AutentificarController : Controller
+    {
+        //
+        // GET: /Autentificar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Autentificar/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Autentificar/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }      
+    }
+}
---
+++ DocsDigitalesApp/DocsDigitalesApp/Models/AutentificarModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DocsDigitalesApp.Models
+{
+    public class AutentificarModel
+    {
+        [Required(ErrorMessage = "Debe capturar su Correo Electronico")]
+        public string correoElectronico { get; set; }
+
+        [Required(ErrorMessage = "Debe capturar su Contraseña")]
+        public string contrasena { get; set; }
+    }
+}
