5a10c6e5c067a58a191b96ed1dbd11e3fb1797f6
plizarraga <plizarraga86@gmail.com>
2014-07-18 14:52:09 -0700

    Registar usuario

--- DocsDigitalesApp/DocsDigitalesApp/Controllers/AutentificarController.cs
+++ DocsDigitalesApp/DocsDigitalesApp/Controllers/AutentificarController.cs
@@ -5,11 +5,15 @@ using System.Web;
 using System.Web.Mvc;
 using DocsDigitalesApp.Models;
 using System.Web.Security;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace DocsDigitalesApp.Controllers
 {
     public class AutentificarController : Controller
     {
+        private string _conString = ConfigurationManager.ConnectionStrings["conMySQL"].ConnectionString.ToString();
+
         // Manda llamar la vista de Login para iniciar sesion
         public ActionResult Login()
         {
@@ -46,21 +50,51 @@ namespace DocsDigitalesApp.Controllers
         // POST: /Autentificar/Create
 
         [HttpPost]
-        public ActionResult Create(UsuarioModel model)
+        public JsonResult Create(UsuarioModel model)
         {
-            try
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                using (MySqlConnection cn = new MySqlConnection(_conString))
                 {
-                    var usuario = model;
-                    
-                }
-                return RedirectToAction("Index", "Home");
+                    var cmd = new MySqlCommand("SP_INS_EMPRESA_USUARIO", cn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("pi_nombre_usuario", model.Nombre.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_correo_electronico", model.CorreoElectronico.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_rfc", model.RFC.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_contrasena", model.Contrasena.Trim());
+                    cmd.Parameters.AddWithValue("pi_nombre_empresa", model.Empresa.Trim().ToUpper());
+
+                    cn.Open();
+
+                    cmd.ExecuteScalar();
+                    cn.Close();
+                    return Json(new { respuesta = "Se registro el usuario y la empresa", error = "" });
+                }               
             }
-            catch
+            else
             {
-                return View();
+                return Json(new { respuesta = "", error = "Modelo no valido" });
             }
-        }      
+
+        }
+
+        // Verificar si existe la empresa
+        [HttpPost]
+        public int IfExistEmpresa(string Nombre)
+        {
+            using (MySqlConnection cn = new MySqlConnection(_conString))
+            {
+                var cmd = new MySqlCommand("SP_SEL_EMPRESA_POR_NOMBRE", cn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("pi_nombre", Nombre);
+                cn.Open();
+
+                int Found = Convert.ToInt32(cmd.ExecuteScalar());
+                cn.Close();
+
+                return Found;
+            }
+        }
     }
 }
