7904cccef1604d329763f1822471032119e21c14
plizarraga <plizarraga86@gmail.com>
2014-07-18 19:51:13 -0700

    Se agrego el model de sucursal y sucursalViewModel

---
+++ DocsDigitalesApp/DocsDigitalesApp/Models/SucursalModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocsDigitalesApp.Models
+{
+    public class SucursalModel
+    {
+        public int id_sucursal { get; set; }
+        public int id_empresa { get; set; }
+        public string nombre { get; set; } 
+        public string calle { get; set; } 
+        public string colonia { get; set; } 
+        public int num_ext { get; set; }
+        public int num_int { get; set; }
+        public int codigo_postal { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+    }
+}
---
+++ DocsDigitalesApp/DocsDigitalesApp/Models/SucursalViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocsDigitalesApp.Models
+{
+    public class SucursalViewModel : SucursalModel
+    {
+        public int no_empleados { get; set; }
+    }
+}
