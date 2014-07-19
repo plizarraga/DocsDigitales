12a2ddc564b7cac23855df64effbd5840699d6d7
plizarraga <plizarraga86@gmail.com>
2014-07-19 09:26:04 -0700

    Se agregaron repositorios de sucursales

--- DocsDigitalesApp/DocsDigitalesApp/Controllers/SucursalController.cs
+++ DocsDigitalesApp/DocsDigitalesApp/Controllers/SucursalController.cs
@@ -13,63 +13,11 @@ namespace DocsDigitalesApp.Controllers
     [Authorize]
     public class SucursalController : Controller
     {
-        private static string _conString = ConfigurationManager.ConnectionStrings["conMySQL"].ConnectionString.ToString();
-
-        //
-        // GET: /Sucursal/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private string _conString = ConfigurationManager.ConnectionStrings["conMySQL"].ConnectionString.ToString();
 
         public ActionResult Sucursales()
         {          
-            return View(GetSucursales(18));
+            return View(SucursalesRepo.GetSucursales(18));
         }
-
-        // Devuelve el listado de sucursales
-        public static IEnumerable<SucursalViewModel> GetSucursales(int id_empresa)
-        {
-            using (MySqlConnection cn = new MySqlConnection(_conString))
-            {
-                cn.Open();
-
-                var cmd = new MySqlCommand("SP_SEL_SUCURSALES_POR_ID_EMPRESA", cn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                
-                cmd.Parameters.AddWithValue("pi_id_empresa", id_empresa);
-
-                var da = new MySqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-
-                cn.Close();
-
-                List<SucursalViewModel> listSucursales= new List<SucursalViewModel>();
-                SucursalViewModel item;
-
-                foreach (DataRow dr in dt.Rows)
-                {
-                    item = new SucursalViewModel();
-
-                    item.id_empresa =    Convert.ToInt32(dr["id_empresa"]);
-                    item.id_sucursal =   Convert.ToInt32(dr["id_sucursal"].ToString());
-                    item.nombre =        dr["nombre"].ToString();
-                    item.calle =         dr["calle"].ToString();
-                    item.colonia =       dr["colonia"].ToString();
-                    item.num_ext =       Convert.ToInt32(dr["num_ext"]);
-                    item.num_int =       Convert.ToInt32(dr["num_int"]);
-                    item.codigo_postal = Convert.ToInt32( dr["codigo_postal"]);
-                    item.ciudad =        dr["ciudad"].ToString();
-                    item.pais =          dr["pais"].ToString();
-                    item.no_empleados =  Convert.ToInt32(dr["no_empleados"]);
-                    listSucursales.Add(item);
-                }
-
-                return listSucursales;
-            }
-        }
-
     }
 }
---
+++ DocsDigitalesApp/DocsDigitalesApp/Models/SucursalesRepo.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace DocsDigitalesApp.Models
+{
+    public class SucursalesRepo
+    {
+        private static string _conString = ConfigurationManager.ConnectionStrings["conMySQL"].ConnectionString.ToString();
+
+        // Devuelve el listado de sucursales
+        public static IEnumerable<SucursalViewModel> GetSucursales(int id_empresa)
+        {
+            using (MySqlConnection cn = new MySqlConnection(_conString))
+            {
+                cn.Open();
+
+                var cmd = new MySqlCommand("SP_SEL_SUCURSALES_POR_ID_EMPRESA", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("pi_id_empresa", id_empresa);
+
+                var da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cn.Close();
+
+                List<SucursalViewModel> listSucursales = new List<SucursalViewModel>();
+                SucursalViewModel item;
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    item = new SucursalViewModel();
+
+                    item.id_empresa = Convert.ToInt32(dr["id_empresa"]);
+                    item.id_sucursal = Convert.ToInt32(dr["id_sucursal"].ToString());
+                    item.nombre = dr["nombre"].ToString();
+                    item.calle = dr["calle"].ToString();
+                    item.colonia = dr["colonia"].ToString();
+                    item.num_ext = Convert.ToInt32(dr["num_ext"]);
+                    item.num_int = Convert.ToInt32(dr["num_int"]);
+                    item.codigo_postal = Convert.ToInt32(dr["codigo_postal"]);
+                    item.ciudad = dr["ciudad"].ToString();
+                    item.pais = dr["pais"].ToString();
+                    item.no_empleados = Convert.ToInt32(dr["no_empleados"]);
+                    listSucursales.Add(item);
+                }
+
+                return listSucursales;
+            }
+        }
+    }
+}
