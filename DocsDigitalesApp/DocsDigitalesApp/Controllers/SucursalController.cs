67b40a07e8f05954de0da30063e2451202a0cc25
plizarraga <plizarraga86@gmail.com>
2014-07-19 19:56:08 -0700

    Add Sucursal

--- DocsDigitalesApp/DocsDigitalesApp/Controllers/SucursalController.cs
+++ DocsDigitalesApp/DocsDigitalesApp/Controllers/SucursalController.cs
@@ -13,11 +13,68 @@ namespace DocsDigitalesApp.Controllers
     [Authorize]
     public class SucursalController : Controller
     {
+        private string _conString = ConfigurationManager.ConnectionStrings["conMySQL"].ConnectionString.ToString();
+
         // Listado de sucursales
         public ActionResult Index()
         {
             UsuarioViwModel usuario = UsuarioRepo.GetUsuario(User.Identity.Name);
             return View(SucursalesRepo.GetSucursales(usuario.Id_Empresa));
+        }
+
+        // Insertar el usuario y la empresa.
+        [HttpPost]
+        public JsonResult AddSucursal(SucursalModel model)
+        {
+            try
+            {
+                using (MySqlConnection cn = new MySqlConnection(_conString))
+                {
+                    UsuarioViwModel usuario = UsuarioRepo.GetUsuario(User.Identity.Name);
+
+                    var cmd = new MySqlCommand("SP_INS_SUCURSAL", cn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("pi_id_empresa", usuario.Id_Empresa);
+                    cmd.Parameters.AddWithValue("pi_nombre", model.nombre.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_calle", String.IsNullOrEmpty(model.calle) ? "" : model.calle.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_colonia", String.IsNullOrEmpty(model.colonia) ? "" : model.colonia.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_num_ext", Convert.ToInt32(model.num_ext));
+                    cmd.Parameters.AddWithValue("pi_num_int", Convert.ToInt32(model.num_int));
+                    cmd.Parameters.AddWithValue("pi_cp", Convert.ToInt32(model.codigo_postal));
+                    cmd.Parameters.AddWithValue("pi_ciudad", String.IsNullOrEmpty(model.ciudad) ? "" : model.ciudad.Trim().ToUpper());
+                    cmd.Parameters.AddWithValue("pi_pais", String.IsNullOrEmpty(model.pais) ? "" : model.pais.Trim().ToUpper());
+
+                    cn.Open();
+
+                    cmd.ExecuteScalar();
+                    cn.Close();
+                    return Json(new { respuesta = "Sucursal registrada exitosamente", error = "" });
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { respuesta = "", error = e.Message.ToString() });
+            }
+        }
+
+        // Verificar si existe la sucursal en la base de datos
+        [HttpPost]
+        public JsonResult IfExistSucursal(string NombreSucursal)
+        {
+            using (MySqlConnection cn = new MySqlConnection(_conString))
+            {
+                // Verificar si existe la empresa
+                var cmd = new MySqlCommand("SP_SEL_SUCURSAL_POR_NOMBRE", cn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("pi_nombre", NombreSucursal);
+                cn.Open();
+
+                int FoundSucursal = Convert.ToInt32(cmd.ExecuteScalar());
+                cn.Close();
+
+                return Json(new { sucursal = FoundSucursal });
+            }
         }
     }
 }
